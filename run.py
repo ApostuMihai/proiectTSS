5a7d6c9cc04df1e9b1bc2c4bfd1a21abe74d91a3
Pavel Alexandru <l3x3yy@gmail.com>
2024-05-12 21:54:45 +0300

    Adaugare run.py

---
+++ run.py
@@ -0,0 +1,5 @@
+from string_manipulator import StringChecker
+# Run the main method in the class
+if __name__ == "__main__":
+    checker = StringChecker()  # Create an instance of StringChecker
+    checker.main()  # Call the main method on the instance
